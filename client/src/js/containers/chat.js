ad6a2d41e3198cca1e630d99cfdb8282577ab11e
Jeremy Evans <openspacehexagon@gmail.com>
2016-12-29 19:32:32 -0500

    improved chat ui; enter key to send

--- client/src/js/containers/chat.js
+++ client/src/js/containers/chat.js
@@ -10,35 +10,87 @@ const styles = {
         minHeight: '2em'
     },
     text: {
-        width: '70%'
+        width: '70%',
+        background: "black",
+        color: "white"
     },
     button: {
+        background: "black",
+        color: "white",
         width: '15%'
+    },
+    message : {
+      display: "block",
+      marginRight: "50%",
+      marginBottom: "0.5em"
+    },
+    innerMessage: {
+      background: "black",
+      color: "white",
+      padding: "0.25em"
+    },
+    messages: {
+      width: "85vw",
+      minWidth: "410px",
+      margin: "auto",
+      textAlign: "left",
+      position: "fixed",
+      left: "2vw",
+      bottom: "7vh"
+    },
+    inputs: {
+      minHeight: "2em",
+      minWidth: "480px",
+      position: "fixed",
+      bottom: "1vh",
+      width: "48vw",
+      textAlign: "left",
+      left: "2vw"
     }
 }
 
 class Chat extends Component {
-  componentDidMount () {
-        this.state = {
-            text: ""
-        }
+  constructor() {
+    super()
+    this.state = {
+        text: ""
+    }
   }
-  send () {
+  componentDidMount () {
+
+  }
+  send (message) {
       console.log("send button")
-      this.props.sendMessage(this.state.text)
+      this.props.sendMessage(message || this.state.text)
+      this.setState({
+        text: ""
+      })
+      this.textInput.value = ""
   }
   render() {
     return (
         <Shell className="chat">
-            <section style={styles.chat}>
+            <section style={styles.messages}>
                 {
                     this.props.messages.map(m => (
-                        <span style={styles.message} >{m}</span>
+                        <span style={styles.message} >
+                          <span style={styles.innerMessage}>
+                            {m}
+                          </span>
+                        </span>
                     ))
                 }
             </section>
-            <section style={styles.input}>
-                <input type='text' onBlur={ (e)=> { this.setState({text: e.target.value }) }} style={styles.text} />
+            <section style={styles.inputs}>
+                <input type='text'
+                       ref={(input) => { this.textInput = input; }}
+                       onBlur={ (e)=> { this.setState({text: e.target.value }) }}
+                       onKeyDown={ (e)=> {
+                         if (e.which == 13) {
+                           this.send(e.target.value)
+                         }
+                       }}
+                       style={styles.text} />
                 <input type='button' onClick={ (e) => { this.send() } } value="Send" style={styles.button} />
             </section>
         </Shell>
