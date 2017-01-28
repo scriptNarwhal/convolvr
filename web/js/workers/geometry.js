89362dbb24c7348d5caecee9d4574e0ba04db1f3
Jeremy Evans <openspacehexagon@gmail.com>
2017-01-28 16:01:13 -0500

    switched from go-json-rest to echo web framework; fixes

---
+++ web/js/workers/geometry.js
@@ -0,0 +1,14 @@
+/* geometry worker */
+self.onmessage = function (event) { // Do some work.
+	let message = JSON.parse(event.data),
+			data = message.data
+  if (message.command == "generate structure") {
+    // implement
+
+    // return typed arrays of vertices
+  }
+};
+
+self.stop = function () {
+	clearTimeout(self.updateLoop);
+}
