82979ed4b601838c16fef2233665a2cab011f97b
Jeremy Evans <openspacehexagon@gmail.com>
2018-09-16 21:26:53 -0400

    cleaning up

--- client/src/systems/logic/virtual-machine.ts
+++ client/src/systems/logic/virtual-machine.ts
@@ -2,8 +2,6 @@
 import Component from '../../core/component.js';
 import Convolvr from '../../world/world'
 
-// import { parser } from 'ecs'
-
 export default class VirtualMachine {
 
     world: Convolvr
@@ -14,42 +12,35 @@ export default class VirtualMachine {
 
     init ( component: Component ) {
 
-        let stack: any[] = []
-        // REFACTOR THIS
-        // first make convolvr/ecs based on SpaceHexagon/ecs
-        // integrate here
-        // detect memory, ioController, gpu,  if available
+        this.detectHardware(component);
 
         return {
-            operate: ( component: Component ) => { // called continually, with a maximum number of operations per second
-                
-                this.operate( component )
-
+            getVirtualDevices: () => {
+                return this.getVirtualDevices(component)
             },
-            onInput: ( component: Component, input: any ) => {
-
-                this.onInput ( component, input )
-
+            addVirtualDevice: (id: string, device: any) => {
+                return this.addVirtualDevice(component, id, device)
             },
-            stack
+            removeVirtualDevice : (id: string) => {
+                return this.removeVirtualDevice(component, id)
+            }
         }
 
     }
 
-    operate ( component: Component ) {
-        let stack = component.state.cpu.stack;
-        // supporting webasm would be ideal.. also interesting would be:
-        
-        // implement some kind of imperative syntax for:
-        // setting, getting vars, calculations, comparisons / if / loops, 
-        // calling & defining functions, with params & return / end of function,
+    private detectHardware(component: Component) {
 
     }
+
+    private getVirtualDevices (component: Component) {
+       
+    }
     
-    onInput ( component: Component, input: any ) {
-        let stack = component.state.cpu.stack;
-        // implement
-        // .. this should use an IOController attr in an adjacent component
+    private addVirtualDevice (component: Component, id: string, data: any) {
+       
+    }
+
+    private removeVirtualDevice(component: Component, id: string) {
 
     }
 
